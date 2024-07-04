97f096aabe1e1240fa7f3a74aa274a2193d1af8e
swelachu <swelachu@gmail.com>
2024-07-04 08:02:07 +0100

    test

--- src/org/demo/BrowserLaunch.java
+++ src/org/demo/BrowserLaunch.java
@@ -1,8 +1,9 @@
 package org.demo;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserLaunch {
@@ -10,12 +11,13 @@ public class BrowserLaunch {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\eclipse-workspace\\SeleniumLaunch\\Driver\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://norsup.nuventuretech.com/login/");
-	//	WebElement userName = driver.findElement(By.id("email"));
-		// userName.sendKeys("Swelachu@gmail.com");
-		// WebElement password = driver.findElement(By.id("pass"));
-		// password.sendKeys("12345");
-		// WebElement login = driver.findElement(By.name("login"));
-		// login.click();
+		 WebElement userName = driver.findElement(By.id("email"));
+		 userName.sendKeys("Swelachu@gmail.com");
+		 WebElement password = driver.findElement(By.id("pass"));
+		 password.sendKeys("12345");
+		 WebElement login = driver.findElement(By.name("login"));
+		 login.click();
+		 System.out.println("swetha");
 		
 		
 	}
